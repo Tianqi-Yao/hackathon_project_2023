41d1a719dc05c7564dbb1adf021e15cde75b3f52
jiayuegou <65645659+jiayuegou@users.noreply.github.com>
2023-06-29 18:01:10 -0500

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Demo from "./components/demoComponent/Demo.js"
-import HomePage from "./components/homePageComponent/HomePage.js"
-import Api from './components/apiComponent/Api.js'
-
-
+import "./App.css";
+import Demo from "./components/demoComponent/Demo.js";
+// import HomePage from "./components/homePageComponent/HomePage.js"
+import HomePage from "./components/pageComponents/homePage/HomePage";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/demo" element={<Demo />} />
-          <Route path="/api" element={<Api />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Demo />
+      <HomePage />
+    </div>
   );
 }
 
---
+++ client/src/components/pageComponents/homePage/HomePage.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Navbar from "../../reusableComponent/navbar/Navbar";
+
+const HomePage = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <h1>home</h1>
+    </div>
+  );
+};
+
+export default HomePage;
