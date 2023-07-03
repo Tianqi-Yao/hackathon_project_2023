35563d317d0637e8cbc50879469c44d3021916fb
jiayuegou <65645659+jiayuegou@users.noreply.github.com>
2023-07-03 18:13:49 -0500

    search bar

--- client/src/components/pageComponents/searchPage/SearchPage.js
+++ client/src/components/pageComponents/searchPage/SearchPage.js
@@ -1,10 +1,12 @@
 import "./SearchPage.css";
 import { useState } from "react";
+import SearchBar from "../../reusableComponent/searchBar/SearchBar";
 
 const SearchPage = () => {
   return (
     <div className="search-page">
-      <h1>search</h1>
+      <h1 className="search-page-title">search</h1>
+      <SearchBar />
     </div>
   );
 };
--- client/src/components/reusableComponent/searchBar/SearchBar.js
+++ client/src/components/reusableComponent/searchBar/SearchBar.js
@@ -1,11 +1,20 @@
 import "./SearchBar.css";
 import { useState } from "react";
+import locationIcon from "../../../assets/images/location.svg";
+import searchIcon from "../../../assets/images/search.svg";
+import micIcon from "../../../assets/images/mic.svg";
 
 const SearchBar = () => {
   return (
     <div className="search-bar">
-      <h1>search bar</h1>
-      <input type="text" placeholder="Search for anything..." />
+      <input
+        className="search-bar-input"
+        type="text"
+        placeholder="Search for anything..."
+      />
+      <img src={searchIcon} alt="Search Icon" className="search-icon" />
+      <img src={micIcon} alt="Mic Icon" className="mic-icon" />
+      <img src={locationIcon} alt="Location Icon" className="location-icon" />
     </div>
   );
 };
