aedde2fcc71981318ecabeb06a6fa30bb9ea3b08
jiayuegou <65645659+jiayuegou@users.noreply.github.com>
2023-07-05 12:14:05 -0500

    search page

--- client/src/components/pageComponents/searchPage/SearchPage.js
+++ client/src/components/pageComponents/searchPage/SearchPage.js
@@ -1,12 +1,19 @@
 import "./SearchPage.css";
 import { useState } from "react";
 import SearchBar from "../../reusableComponent/searchBar/SearchBar";
+import Navbar from "../../reusableComponent/navbar/Navbar";
 
 const SearchPage = () => {
   return (
     <div className="search-page">
-      <h1 className="search-page-title">search</h1>
-      <SearchBar />
+      <Navbar />
+      <main className="search-page-content">
+        <div className="search-page-title-container">
+          <h1 className="search-page-title">Customized Food Map with </h1>
+          <h1 className="search-page-subtitle">Nutritional Info.</h1>
+        </div>
+        <SearchBar />
+      </main>
     </div>
   );
 };
