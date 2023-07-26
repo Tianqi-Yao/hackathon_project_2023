5c792c518806503d2a9d6ed08152cf8e41ba3306
jiayuegou <65645659+jiayuegou@users.noreply.github.com>
2023-07-25 20:11:55 -0500

    map page, homepage, navbar, search bar, reducer

--- client/src/components/reusableComponent/searchBar/SearchBar.js
+++ client/src/components/reusableComponent/searchBar/SearchBar.js
@@ -3,21 +3,49 @@ import { useState } from "react";
 import locationIcon from "../../../assets/images/location.svg";
 import searchIcon from "../../../assets/images/search.svg";
 import micIcon from "../../../assets/images/mic.svg";
+import smallSearchIcon from "../../../assets/images/search-small.svg";
+import smallMicIcon from "../../../assets/images/mic-small.svg";
+import smallLocationIcon from "../../../assets/images/location-small.svg";
 import { useNavigate } from "react-router-dom";
 
-const SearchBar = () => {
+const SearchBar = ({ size }) => {
   const navigate = useNavigate();
   return (
-    <div className="search-bar">
+    <div className={`search-bar${size ? `-${size}` : ""}`}>
       <input
-        className="search-bar-input"
+        className={`search-bar-input${size ? `-${size}` : ""}`}
         type="text"
         placeholder="Search for anything..."
-        onKeyDown={(e) => {e.key === 'Enter' && navigate('/map')}}
+        onKeyDown={(e) => {
+          e.key === "Enter" && navigate("/map");
+        }}
       />
-      <img src={searchIcon} alt="Search Icon" className="search-icon" />
-      <img src={micIcon} alt="Mic Icon" className="mic-icon" />
-      <img src={locationIcon} alt="Location Icon" className="location-icon" />
+
+      {size ? (
+        <>
+          <img
+            src={smallSearchIcon}
+            alt="Search Icon"
+            className="search-icon-small"
+          />
+          <img src={smallMicIcon} alt="Mic Icon" className="mic-icon-small" />
+          <img
+            src={smallLocationIcon}
+            alt="Location Icon"
+            className="location-icon-small"
+          />
+        </>
+      ) : (
+        <>
+          <img src={searchIcon} alt="Search Icon" className="search-icon" />
+          <img src={micIcon} alt="Mic Icon" className="mic-icon" />
+          <img
+            src={locationIcon}
+            alt="Location Icon"
+            className="location-icon"
+          />
+        </>
+      )}
     </div>
   );
 };
